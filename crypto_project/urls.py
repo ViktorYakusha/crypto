bf3bd5f48965116d4b481b472c057f2c57d60dbb
ViktorYakusha <viktor.yakusha@gmail.com>
2025-08-02 17:07:51 +0300

    update admin url

--- crypto_project/urls.py
+++ crypto_project/urls.py
@@ -1,3 +1,4 @@
+import os
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path
@@ -8,5 +9,5 @@ from .apps.main import views as main_views
 
 urlpatterns = [
     path('', main_views.homepage),
-    path('admin/', admin.site.urls),
+    path(os.getenv('ADMIN_URL'), admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + debug_toolbar_urls()
