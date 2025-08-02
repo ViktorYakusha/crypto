3344ae66c8e68743908695a81e7cc55d77266b51
ViktorYakusha <viktor.yakusha@gmail.com>
2025-08-02 16:48:23 +0300

    add main template

--- crypto_project/urls.py
+++ crypto_project/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+from .apps.main import views as main_views
+
 urlpatterns = [
+    path('', main_views.homepage),
     path('admin/', admin.site.urls),
-] + debug_toolbar_urls()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + debug_toolbar_urls()
