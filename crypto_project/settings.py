4680aff995eb1b3dff60fdae99618b8da11bddac
viktor <viktor.yakusha@gmail.com>
2025-09-13 07:15:14 +0300

    update settings

--- crypto_project/settings.py
+++ crypto_project/settings.py
@@ -17,6 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "secret")
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+INTERNAL_IPS = ['127.0.0.1']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin'
 
@@ -123,7 +124,13 @@ TIME_ZONE = 'Europe/Kyiv'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
