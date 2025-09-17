7937837131abccca35c284c33aeedfdc8a201ffc
viktor <viktor.yakusha@gmail.com>
2025-09-17 17:41:33 +0300

    update profile

--- crypto_project/customer/admin.py
+++ crypto_project/customer/admin.py
@@ -4,6 +4,7 @@ from .models import Customer, Manager, Comment
 
 
 class CustomerAdmin(admin.ModelAdmin):
+    exclude = ('user',)
     list_display = ['user__name', 'user__email', 'manager__user__name', 'balance', 'is_verified']
     list_filter = ['manager__user__name']
 
@@ -11,12 +12,14 @@ admin.site.register(Customer, CustomerAdmin)
 
 
 class ManagerAdmin(admin.ModelAdmin):
+    exclude = ('user',)
     list_display = ['user__name', 'user__email']
 
 admin.site.register(Manager, ManagerAdmin)
 
 
 class CommentAdmin(admin.ModelAdmin):
+    exclude = ('customer', 'author',)
     list_display = ['customer__user__name', 'author__user__name', 'created_date']
     list_filter = ['customer__user__name', 'author__user__name']
 
